8b3101717dc12c3787f79f428c653c6b2364536a
yuma-o1412 <yo.business.ac@gmail.com>
2023-11-29 16:02:43 +0900

    first commit

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+export default function Profile() {
+    return (
+        <main>
+            <div>
+                <p>
+                    私は学生です。プログラミングを勉強しています。
+                    頑張りますのでよろしくお願いいたします。git
+                </p>
+            </div>
+        </main>
+    )
+}
